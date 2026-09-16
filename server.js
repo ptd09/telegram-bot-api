dc40505cd90fe8eba0fcb6ad43393d309fa45e15
ptd09 <flo140613@gmail.com>
2026-09-16 21:08:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const axios = require('axios');
 const FormData = require('form-data');
 
 const app = express();
+const cors = require('cors');
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Drive-Token']
+}));
+
+// Cho phép xử lý request OPTIONS trên mọi đường dẫn
+app.options('*', cors());
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
